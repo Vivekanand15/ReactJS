9b8ff368e48cf626801fee352f245a01d6823862
Vivekanand <vivekanand.v@surya-soft.com>
2022-02-19 22:38:47 +0530

    get the season by making another function in which we are passing lat and month it is return the season as winter or summer

--- tester/src/SeasonDisplay.js
+++ tester/src/SeasonDisplay.js
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react";
 
+
+const getSeason=(lat,month)=>{
+    if(month>2 && month<9){
+        return lat>0?'summer':'winter';
+    }
+    else{
+        return lat>0?'winter':'summer';
+    }
+}
+
 const SeasonDispaly=(props)=>{
-    console.log(props.lat);
-    return <div>Season Display: {props.lat}</div>
+    const season=getSeason(props.lat,new Date().getTime());
+    console.log(season);
+    return <div>Season Display</div>
 }
 
 export default SeasonDispaly;
