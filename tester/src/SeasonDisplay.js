f845db0ba2e59822710b9a1056d14755955320e5
Vivekanand <vivekanand.v@surya-soft.com>
2022-02-19 23:34:02 +0530

    we priviousally written the two ternary operator for text and icon int the we change the icon as iconName and text and icon in a object

--- tester/src/SeasonDisplay.js
+++ tester/src/SeasonDisplay.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react";
 
-
+const seasonConfig={
+    summer:{
+        text:"Let's hit the beach",
+        iconName:"sun"
+    },
+    winter:{
+        text:"Burr, it is chilly",
+        iconName:"snowflake"
+    }
+}
 const getSeason=(lat,month)=>{
     if(month>2 && month<9){
         return lat>0?'summer':'winter';
@@ -13,13 +22,11 @@ const getSeason=(lat,month)=>{
 
 const SeasonDispaly=(props)=>{
     const season=getSeason(props.lat,new Date().getTime());
-    const text=season==='winter'? 'Burr, it is chilly':'Lets hit the beach';
-    const icon=season==='winter'?'snowflake':'sun';
-    
+    const {text,iconName}=seasonConfig[season];
     return <div>
-        <i className={`${icon} icon`} />
+        <i className={`${iconName} icon`} />
         <h1>{text}</h1>
-        <i className={`${icon} icon`} />
+        <i className={`${iconName} icon`} />
 
 
     </div>
