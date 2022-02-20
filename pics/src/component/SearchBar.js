7463b4c32486346c58aa75825485a14626959fee
Vivekanand <vivekanand.v@surya-soft.com>
2022-02-20 15:55:49 +0530

    we added the controlled element over uncontrolled

--- pics/src/component/SearchBar.js
+++ pics/src/component/SearchBar.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class SearchBar extends React.Component{
+    state={term:"hi there.!"};
 
-
-    onInputChange(event){
-        console.log(event.target.value);
-    }
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    // }
 
     render(){
         return <div className="ui segment">
             <form className="ui form">
                 <div className="field">
                 <label>Search Image</label>
-                <input type='text' onChange={this.onInputChange} />
+                <input type='text' value={this.state.term} onChange={(e)=>this.setState({term:e.target.value})} />
                 </div>
             </form>
         </div>
