ae1e067454b3966d9fffb65d9598ebd0df1e4075
Vivekanand <vivekanand.v@surya-soft.com>
2022-02-20 14:19:17 +0530

    we added the event handler 'onChange' which return whenever the input is change

--- pics/src/component/SearchBar.js
+++ pics/src/component/SearchBar.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class SearchBar extends React.Component{
+
+
+    onInputChange(event){
+        console.log(event.target.value);
+    }
+
     render(){
         return <div className="ui segment">
             <form className="ui form">
                 <div className="field">
                 <label>Search Image</label>
-                <input type='text'/>
+                <input type='text' onChange={this.onInputChange} />
                 </div>
             </form>
         </div>
