ad1307d04929ac8db1e2e6d5a9f88e6b3343a8cb
Vivekanand <vivekanand.v@surya-soft.com>
2022-02-23 04:28:49 +0530

    unspalsh

--- pics/src/component/App.js
+++ pics/src/component/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from "axios";
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
     state={image:[]};
     onSearchSubmit=async (term)=>{
-        const respons= await axios.get('https://api.unsplash.com/search/photos',{
-        params:{query: term },
-        headers:{
-            Authorization:'Client-ID xA8AfT_Sc2rHfgLM65IvrUR1t7Y2wlKAcy5eVGwoSuY'
-        }
+        const respons= await unsplash.get('/search/photos',{
+        params:{query: term },   
     });
+
     this.setState({image:respons.data.results});
     }
+    
     render(){
     return <div className='ui container' style={{marginTop:'10px'}}>
         <SearchBar  onSubmit={this.onSearchSubmit}/>
